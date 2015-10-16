b5b84c4a9794fd4973cd3b93ec3449b6b97eafbb
asaf golan <asafgolan@asafs-MacBook-Pro.local>
2015-10-16 16:25:45 +0300

    hey

---
+++ Assets/Scripts/PlayerNetworkSetup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerNetworkSetup : NetworkBehaviour {
+
+	[SerializeField] Camera FPScam;
+	[SerializeField] AudioListener audioListener;
+
+	// Use this for initialization
+	void Start ()
+	{
+		if(isLocalPlayer)
+		{
+			GameObject.Find ("Main Camera").SetActive(false);
+			GetComponent<CharacterController>().enabled = true ;
+			GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = true;
+			FPScam.enabled = true ;
+			audioListener.enabled = true ;
+
+
+		}	
+	}
+	
+
+}
